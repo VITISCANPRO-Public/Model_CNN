7ccf6f74905c75c4e660382b148976b9375f04b0
Guillaume VIEL <guillaume.viel@gmail.com>
2026-02-06 02:03:46 +0100

    fix pb test conversion device en string

--- tests/test_model.py
+++ tests/test_model.py
@@ -3,4 +3,5 @@ from scripts.model import get_device, create_model_transfer_learning, create_mod
 
 def test_get_device():
     device = get_device()
-    assert device.lower() in [ "cpu", "cuda", "mps"], "Device in 'cpu', 'cuda', 'mps'"
+    device_str = str(device).lower()
+    assert device_str in [ "cpu", "cuda", "mps"], f"Device must be 'cpu', 'cuda' or 'mps', but got '{device_str}'"
